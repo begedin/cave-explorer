f74e85bc6d81a708fbc18005ca38742b259bca14
Nikola <begedinnikola@gmail.com>
2017-02-27 19:28:44 +0100

    Add forward acceleration

--- src/sprites/mushroom.js
+++ src/sprites/mushroom.js
@@ -5,17 +5,28 @@ export default class extends Phaser.Sprite {
   constructor ({ game, x, y }) {
     super(game, x, y, 'mushroom');
     this.anchor.setTo(0.5);
-    this.game.physics.enable([ this ], Phaser.Physics.ARCADE);
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+
+    this.body.drag.set(50);
+    this.body.maxVelocity.set(300);
+
+    this.body.angularDrag = 200;
+    this.body.maxAngular = 200;
   }
 
   update () {
-    this.body.angularAcceleration = 0;
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      this.game.physics.arcade.accelerationFromRotation(this.rotation - Math.PI/2, 200, this.body.acceleration);
+    } else {
+      this.body.acceleration.set(0);
+    }
 
     if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        this.body.angularAcceleration -= 200;
-    } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-    {
-        this.body.angularAcceleration += 200;
+      this.body.angularAcceleration -= 50;
+    } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      this.body.angularAcceleration += 50;
+    } else {
+      this.body.angularAcceleration = 0;
     }
   }
 }
