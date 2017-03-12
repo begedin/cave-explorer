d830f1c932512e63c06e20a3539283b661d5762f
Nikola Begedin <begedinnikola@gmail.com>
2017-03-12 12:38:46 +0100

    Scale down player

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,10 @@ export const centerGameObjects = (objects) => {
     object.anchor.setTo(0.5);
   });
 };
+
+export const scaleObjectToGameSize = (object, factor = 1) => {
+  const gameTileSize = 32;
+  let dimension = object.height > object.width ? object.height : object.width;
+  let scale = gameTileSize / dimension * factor;
+  object.scale.setTo(scale);
+};
